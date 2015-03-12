51e42d4e8898a3487cc9a82997dc7a71c10041d7
bluelovers <codelovers@users.sourceforge.net>
2015-03-13 01:55:02 +0800

    lib (Step: 6)
    Step: 6
    
    [A ] new file:
    
    	.gitkeep
    	common.pl
    	func.pl
    	git.pm
    	input.pl
    	php.pl

---
+++ msysgit/local/bin/lib/php.pl
@@ -0,0 +1,47 @@
+
+# Perl trim function to remove whitespace from the start and end of the string
+sub trim($)
+{
+	my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	return $string;
+}
+
+# Left trim function to remove leading whitespace
+sub ltrim($)
+{
+	my $string = shift;
+	$string =~ s/^\s+//;
+	return $string;
+}
+
+# Right trim function to remove trailing whitespace
+sub rtrim($)
+{
+	my $string = shift;
+	$string =~ s/\s+$//;
+	return $string;
+}
+
+sub is_number
+{
+#	return looks_like_number(shift);
+
+	return defined $_[0] && $_[0] =~ /^[+-]?\d+$/;
+}
+
+sub in_array
+{
+     my ($arr, $search_for) = @_;
+     return grep {$search_for eq $_} @$arr;
+}
+
+sub is_number
+{
+#	return looks_like_number(shift);
+
+	return defined $_[0] && $_[0] =~ /^[+-]?\d+$/;
+}
+
+1;
