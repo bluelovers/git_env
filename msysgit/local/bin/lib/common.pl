022f457398c17caa19269711a7b3291e7b3e44d0
bluelovers <codelovers@users.sourceforge.net>
2015-03-13 17:34:27 +0800

    . (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	git-ioc
    	lib/input.pm
    
    [ M] modified - Changes not staged for commit:
    
    	lib/common.pl
    	lib/git.pm
    	lib/php.pl
    
    [ D] deleted - Changes not staged for commit:
    
    	lib/input.pl

--- msysgit/local/bin/lib/common.pl
+++ msysgit/local/bin/lib/common.pl
@@ -1,5 +1,14 @@
 
+use File::Basename qw(dirname);
+
 use constant false => 0;
 use constant true  => 1;
+
+use constant LAZYLIBPATH  => dirname(__FILE__);
+
+unshift @INC, LAZYLIBPATH;
+
+require 'php.pl';
+require 'func.pl';
 
 1;
