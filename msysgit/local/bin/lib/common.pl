d070ee17a2624d9eb45a96260e872d439776d18e
bluelovers <codelovers@users.sourceforge.net>
2015-03-14 19:42:43 +0800

    . (Step: 4)
    Step: 4
    
    [M ] modified:
    
    	msysgit/local/bin/lib/input.pm
    	msysgit/local/bin/lib/php.pl
    
    [A ] new file:
    
    	msysgit/local/bin/.git-ignoreoncommit.sh
    
    [ M] modified - Changes not staged for commit:
    
    	msysgit/local/bin/lib/common.pl
    	msysgit/local/bin/lib/file.pl
    	msysgit/local/bin/lib/git.pm

--- msysgit/local/bin/lib/common.pl
+++ msysgit/local/bin/lib/common.pl
@@ -1,12 +1,15 @@
 
 use File::Basename qw(dirname);
 
-use constant false => 0;
-use constant true  => 1;
+use constant false			=> 0;
+use constant true			=> 1;
 
-use constant LAZYLIBPATH  => dirname(__FILE__);
+use constant LAZYLIBPATH	=> dirname(__FILE__);
 
 unshift @INC, LAZYLIBPATH;
+
+our $false	= false;
+our $true	= true;
 
 require 'php.pl';
 require 'func.pl';
